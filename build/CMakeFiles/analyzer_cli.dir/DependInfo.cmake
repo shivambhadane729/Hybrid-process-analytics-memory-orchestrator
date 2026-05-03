e366c3442a2e74ae60d9db7d84a3b5fec2805472
shivambhadane729 <shivambhadane729@gmail.com>
2026-05-03 10:11:10 +0530

    chore: add build directory and reference repository to the project

---
+++ build/CMakeFiles/analyzer_cli.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/shivam/Desktop/adaptive-os-memory-engine/main_cli.cpp" "CMakeFiles/analyzer_cli.dir/main_cli.cpp.o" "gcc" "CMakeFiles/analyzer_cli.dir/main_cli.cpp.o.d"
+  "" "analyzer_cli" "gcc" "CMakeFiles/analyzer_cli.dir/link.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/analyzer_cli.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/analyzer_cli.dir/link.d"
+  "CMakeFiles/analyzer_cli.dir/main_cli.cpp.o"
+  "CMakeFiles/analyzer_cli.dir/main_cli.cpp.o.d"
+  "analyzer_cli"
+  "analyzer_cli.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/analyzer_cli.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
